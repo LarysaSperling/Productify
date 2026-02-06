4d8247f0fb570b37816ee7c2436c45af6a058d8b
Larysa <lorasperling@gmail.com>
2026-02-06 12:00:01 +0100

    feat: add icons

---
+++ src/components/headerHero/index.jsx
@@ -0,0 +1,6 @@
+import styles from "./HeaderHero.module.css";
+import { ReactComponent as Logo } from "../../assets/logo-circle.svg";
+import { ReactComponent as Facebook } from "../../assets/facebook.svg";
+import { ReactComponent as Telegram } from "../../assets/telegram.svg";
+import { ReactComponent as Twitter } from "../../assets/twitter.svg";
+import { ReactComponent as Medium } from "../../assets/medium.svg";
